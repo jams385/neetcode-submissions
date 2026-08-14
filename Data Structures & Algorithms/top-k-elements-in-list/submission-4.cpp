995eca1c48db094b1fcb07edee8efc3962ce3c24
jams <jamilpalma704@gmail.com>
2026-08-14 13:46:17 +0800

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        unordered_map<int, int> count_map;
+
+        for (const auto& num : nums){
+            count_map[num]++;
+        }
+
+        vector<vector<int>> count_vec;
+
+        for(int i = 0; const auto& [key, value] : count_map){
+            count_vec.push_back({value, key});
+            i++;
+        }
+
+        sort(count_vec.begin(), count_vec.end());
+
+        vector<int> result;
+
+        for (int i = 0; i < k; i++){
+            result.push_back(count_vec.back()[1]);
+            count_vec.pop_back();
+        }
+
+        return result;
+    }
+};
